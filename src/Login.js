7f9f0ee544cf428c2cc0755624ebe66bb289e13d
unknown <meyer.max411@gmail.com>
2023-02-27 00:01:17 +0100

    page choix login fait

--- src/Login.js
+++ src/Login.js
@@ -4,19 +4,19 @@ import "./Login.css"
 function Login() {
   return (
     <>
-      <header>
-        <h1>Burger</h1>
-      </header>
-      <body>
-        <div className="boutton">
-          <div className="client">
-            <button className="client_boutton">Client</button>
+      <div className="body">
+        <header>
+          <h1>Burger</h1>
+        </header>
+          <div className="boutton">
+            <div className="client">
+              <button className="client_boutton">Client</button>
+            </div>
+            <div className="Restaurateur">
+            <button className="Restaurateur_boutton">Restaurateur</button>
+            </div>
           </div>
-          <div className="Restaurateur">
-          <button className="Restaurateur_boutton">Restaurateur</button>
-          </div>
-        </div>
-      </body>
+      </div>
     </>
   );
 }
