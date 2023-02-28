0a6caa2fa7130a94f2c03a3455262204662ea235
patigny.nicolas <Niiyugitowaifu1>
2023-02-28 16:28:53 +0100

    Link ajouter

---
+++ src/Login/ClientLogin.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./ClientLogin.css";
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // add logic for handling login
+    // for example, you could send a request to your backend to authenticate the user
+    // and set the isAuthenticated state to true
+    setIsAuthenticated(true);
+  };
+
+  if (isAuthenticated) {
+    navigate("/Acceuilclient");
+  }
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            required
+          />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/Login/RestaurateurLogin.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function RestaurateurLogin() {
+  return (
+    <>
+      <h1>Restaurateur Login</h1>
+    </>
+  );
+}
+
+export default RestaurateurLogin;
