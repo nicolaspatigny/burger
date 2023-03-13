5b56abe981e1099734a19e7ac68ab37b23e6d469
patigny.nicolas <Niiyugitowaifu1>
2023-03-13 10:23:52 +0100

    store js

--- src/store.js
+++ src/store.js
@@ -24,25 +24,33 @@ const initialState = {
       minQuantity: 0,
     },
   ],
-  total_price: 5.97,
+  base_price: 8.5,
+  total_price: 0, // initial total price includes base price and ingredient prices
 };
 
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
     case "UPDATE_INGREDIENTS":
+      const newIngredients = action.payload;
+      const newTotalPrice =
+        state.base_price + calculateIngredientsTotalPrice(newIngredients);
       return {
         ...state,
-        ingredients: action.payload,
-      };
-    case "UPDATE_TOTAL_PRICE":
-      return {
-        ...state,
-        total_price: action.payload,
+        ingredients: newIngredients,
+        total_price: newTotalPrice,
       };
     default:
       return state;
   }
 };
+
+function calculateIngredientsTotalPrice(ingredients) {
+  let total_price = 0;
+  ingredients.forEach((ingredient) => {
+    total_price += ingredient.price * ingredient.quantity;
+  });
+  return total_price;
+}
 
 const store = createStore(rootReducer);
 
