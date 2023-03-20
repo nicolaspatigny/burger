8406856e28a5109a3455de13faf15fc5e9957b42
meyer.max <meyer.max411@gmail.com>
2023-03-20 07:51:44 +0100

    n ième conflits

--- src/client/Acceuil_client.js
+++ src/client/Acceuil_client.js
@@ -1,10 +1,57 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
-import "./Acceuil_client.css";
-import Profil from "../img/profil.png";
+import Profil_src from "../img/profil.png";
 import MaxiBestOf from "../img/Maxi-bestof.jpg";
 import { Link } from "react-router-dom";
 import Panier from "../panier/Panier";
+import styled from "styled-components";
+
+const Header = styled.header`
+  position: relative;
+  margin-bottom: 10px;
+  height: 75px;
+  background-color: #fff7e9;
+`;
+const Bienvenue = styled.h1`
+  text-align: justify;
+  width: 500px;
+  display: inline-block;
+  margin: 0;
+  font-style: italic;
+`;
+const Profil = styled.img`
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 65px;
+  z-index: 2;
+`;
+
+const Ligne = styled.div`
+  width: 100%;
+  height: 1px;
+  background-color: #fff7e9;
+  position: absolute;
+  left: 0;
+  z-index: 1;
+`;
+
+const Ajouter = styled.button`
+  height: 30px;
+  width: 80px;
+  background-color: rgb(0, 160, 0);
+  color: white;
+
+  &:hover {
+    color: black;
+  }
+`;
+
+const Maxibestof = styled.div`
+  position: relative;
+  left: 10%;
+  padding-left: 100px;
+`;
 
 function Acceuilclient() {
   const location = useLocation();
@@ -13,20 +60,22 @@ function Acceuilclient() {
   return (
     <>
       <div>
-        <header className="header">
-          <h1 className="h1">Bienvenue {username},</h1>
-          <img src={Profil} alt="Profil" className="profil"></img>
-        </header>
+        <Header>
+          <Bienvenue> Bienvenue {username},</Bienvenue>
+          <Profil src={Profil_src} alt="Profil" className="profil"></Profil>
+        </Header>
       </div>
-      <div className="ligne"></div>
+      <Ligne></Ligne>
       <div>
         <body>
           <Link to="/DetailProduits">
             <img src={MaxiBestOf} alt="MaxiBestof" className="MaxiBestof"></img>
           </Link>
-          <div>
-            <Panier />
-          </div>
+          <Maxibestof>
+            Menu Maxi Bestof
+            <Ajouter>+</Ajouter>
+          </Maxibestof>
+          <Panier />
         </body>
       </div>
     </>
--- src/panier/Panier.js
+++ src/panier/Panier.js
@@ -11,7 +11,7 @@ const Footer = styled.footer`
   align-items: center;
   height: 80px;
   padding: 0 20px;
-  background-color: #86deff;
+  background-color: #fff7e9;
 `;
 
 const Total = styled.h1`
