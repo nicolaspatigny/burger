959ef3079bc789fdaba2d497b43e6bceb7555f9c
meyer.max <meyer.max411@gmail.com>
2023-04-07 17:29:12 +0200

    conflits

--- src/restaurateur/Acceuil_restaurateur.js
+++ src/restaurateur/Acceuil_restaurateur.js
@@ -2,24 +2,13 @@ import React from "react";
 import Commandes from "./Commandes";
 import Header from "./Header";
 
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding-top: 100px;
-`;
-
-const Title = styled.h1`
-  margin-bottom: 50px;
-`;
-
-function Accueilrest() {
+function Acceuilrest() {
   return (
-    <Wrapper>
-      <Title>Orders</Title>
+    <body>
+      <Header />
       <Commandes />
-    </Wrapper>
+    </body>
   );
 }
 
-export default Accueilrest;
+export default Acceuilrest;
